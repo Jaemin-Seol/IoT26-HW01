83c9f754ce59aaa9ebed2fa7dbbff4eef8947a15
sbmax002 <jwooooy002@gmail.com>
2026-05-10 17:36:09 +0900

    chore: add comments

--- IoT26-HW01_Cho-Wooyoung.py
+++ IoT26-HW01_Cho-Wooyoung.py
@@ -1,11 +1,14 @@
+# gpiozero: controls raspberrypi pins
 from gpiozero import LED
 from time import sleep
 from signal import pause
 
+# led assign gpio pin 14
 led = LED(14)
 
+# blink 10 times: 1s on, 1s off. running in background
 led.blink(on_time=1, off_time=1, n=10, background=True)
 
+# keeps the process alive while the led blinks in the background
 pause()
-
 
